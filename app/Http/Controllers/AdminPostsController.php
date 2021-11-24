012f82590d18728b268678a5b76f463ad7e88afb
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2021-11-24 09:13:35 +0800

    CRUD_練習6-1:AdminPostsController.php的edit內取得舊資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -19,8 +19,8 @@ class AdminPostsController extends Controller
 
     public function edit($id)
     {
-        $data = ['id' => $id];
-
+        $post=Post::find($id);
+        $data = ['post' => $post];
         return view('admin.posts.edit', $data);
     }
     public function store(Request $request)
