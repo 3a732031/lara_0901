1e198f8a7be7548d47944665683d9a5dff3b69da
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2021-12-15 10:34:21 +0800

    CRUD_練習 8-3:完成posts/{id}路由所串接之PostsController的show方法

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,8 +14,9 @@ class PostsController extends Controller
 
     public function show($id)
     {
-        $data = ['id' => $id];
-
+//        $data = ['id' => $id];
+        $post=Post::find($id);
+        $data = ['post' => $post];
         return view('posts.show', $data);
     }
 }
